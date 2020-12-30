7d87dfd9c0feef2c0c19cd914462c0b3b07eec65
carlosfabila <fabilacarlos54@gmail.com>
2020-12-30 11:01:37 -0800

    Added interface.py to test out classes with Tkinter. Moved my code around in main.py and changed alot of comments. The code looks different now.

--- main.py
+++ main.py
@@ -24,56 +24,74 @@ root = Tk()
 root.title("Probability Calculator")
 root.geometry("350x300")
 
-# We create a frame for our window to use the newer ttk code
-# . This can be one of: flat (default), raised, sunken, solid, ridge, or groove.
-add_to_hat_frame = ttk.Frame(root, borderwidth=4, relief="ridge", padding=5)
-add_to_hat_frame.grid(column=0, row=0)
-
-# This is the title label inside of my frame for adding balls into the hat.
-# We put it into the top left corner of the program.
-title_label = ttk.Label(add_to_hat_frame, text="Choose a color for the ball.\n"
-                                               "Add any amount you want.\n"
-                                               "Hit the ADD button to add the ball(s) to the hat!")
-title_label.grid(row=0, column=0, columnspan=2)
-
-# We need to create the entry field for adding items into our interface as well as the add button.
-# We add them to the grid immediately. After each creation and addition to the grid, I will make a label to the
-# input boxes so it is clear what the user has to enter.
-
-# This will be a string entry and will only accept string. We will write that in a different function.
-color = StringVar()
-color.set("Red")
-color_to_add = ttk.Combobox(add_to_hat_frame, textvariable=color, state="readonly", width=10,
-                            values=("Red", "Blue", "Green", "Yellow", "Pink", "White", "Black", "Brown", "Grey",
-                                    "Purple", "Orange", "Silver", "Violet", "Gold",)).grid(row=1, column=0)
-# color_to_add.bind('<<ComboboxSelected>>', function)
-color_label = ttk.Label(add_to_hat_frame, text="Color of Ball").grid(row=1, column=1)
-
-# This will be a integer entry, and will accept any number.
-# Later I will add a limit.
-amount = IntVar()
-amount_to_add = ttk.Entry(add_to_hat_frame, textvariable=amount, width=10).grid(row=2, column=0)
-amount_label = ttk.Label(add_to_hat_frame, text="Amount to Add").grid(row=2, column=1)
-
-# And this is the final widget inside of this frame. It is an add button that takes the entry from both inputs and
-# sends it to the Hat class's function to add contents to the hat.
-add_button = ttk.Button(add_to_hat_frame, text="Add", command=lambda: click_add_button()).grid(row=3, column=0,
-                                                                                               columnspan=2)
 # This is the frame for the next part of the program. This part requires the user to create a list of what they expect
 # the outcome of the random draw should be. The list will be continually updated and displayed on the right hand side
 # in a new frame. I need to learn how to make my frames the same size to make it look better, or maybe get rid of the
 # visual part of the frames and add these steps, that the user must do to make the program run, into a continuously
 # updating frame, where the user hits a "next" button and the screen changes and you're on to the next step,
-what_to_draw_frame = ttk.Frame(root, borderwidth=4, relief="ridge", padding=5)
-what_to_draw_frame.grid(row=1, column=0, columnspan=2)
 
+
+
+
+"""
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+content.columnconfigure(0, weight=3)
+content.columnconfigure(1, weight=3)
+content.columnconfigure(2, weight=3)
+content.columnconfigure(3, weight=1)
+content.columnconfigure(4, weight=1)
+content.rowconfigure(1, weight=1)
+"""
+
+# We need to move this code around because its making it hard for me to move see where certain things are. I need to
+# group by categories such as, creation of widgets, adding them to the grid and so on. More on the comments ahead.
+
+# This is variable creation for the widgets "textvariables" and other actions
+color = StringVar().set("Red")  # for color_to_add widget
+amount = IntVar()  # for amount_to_add widget
+color_list = ["Red", "Blue", "Green", "Yellow", "Pink", "White", "Black", "Brown", "Grey", "Purple", "Orange",
+              "Silver", "Violet", "Gold"]
+
+# Here we will have all the widgets being created for the program. I need to keep these seperate in case I need to
+# change any of the parameters for the widgets.
+# This holds the first set of options that we give the user. It is to hold all the widgets pertaining to adding items to
+# the hat object.
+add_to_hat_frame = ttk.Frame(root, borderwidth=4, relief="ridge", padding=5)
+# This label gives the user the instructions for the first frame.
+instructions_one = ttk.Label(add_to_hat_frame, text="Choose a color for the ball.\n Add any amount you want.\n Hit the "
+                                                    "ADD button to add the ball(s) to the hat!")
+# This is the dropdown menu with the list of colors that the user may add. I did this so I didn't have to deal with
+# string inputs. Might reduce number of  balls as it is just a small program and the list is large as it is
+color_to_add = ttk.Combobox(add_to_hat_frame, textvariable=color, state="readonly", width=10, values=color_list)
+# A label to go alongside the dropdown menu that we created above.
+color_label = ttk.Label(add_to_hat_frame, text="Color of Ball")
+# This will be a integer entry by the user. Will be used to add items to the hat. Will add limit later.
+amount_to_add = ttk.Entry(add_to_hat_frame, textvariable=amount, width=10)
+# A label to go alongside the "amount" entry widget that we created above.
+amount_label = ttk.Label(add_to_hat_frame, text="Amount to Add")
+# Our add button that confirms the users addition to the hat. Will call on click_to_add function created above.
+add_button = ttk.Button(add_to_hat_frame, text="Add", command=lambda: click_add_button())
+# This is our next frame that hold our second set of instructions
+what_to_draw_frame = ttk.Frame(root, borderwidth=4, relief="ridge", padding=5)
+# The second set of instructions.
 draw_label = ttk.Label(what_to_draw_frame, text="Which color of balls do you expect to be drawn?\nThe drawing list "
                                                 "is updated on the side ==>")
 
+
+# This is where all the variables will be added to the grid. Any further grid configuration for the widgets will
+# probably put here as well unless they become a nuisance and I place that code elsewhere.
+add_to_hat_frame.grid(column=0, row=0)
+instructions_one.grid(row=0, column=0, columnspan=2)
+color_to_add.grid(row=1, column=0)
+color_label.grid(row=1, column=1)
+amount_to_add.grid(row=2, column=0)
+amount_label.grid(row=2, column=1)
+add_button.grid(row=3, column=0, columnspan=2)
+
+
+what_to_draw_frame.grid(row=1, column=0, columnspan=2)
 draw_label.grid(row=0, column=0, columnspan=2)
-
-
-
 
 
 root.mainloop()
